aeb080f9d53cb2dc85e344c8c7e33c5031cc1299
matheusjulidori <matheus_julidori@yahoo.com.br>
2024-04-02 20:53:28 -0300

    :test_tube: Test: Teste de conversão

---
+++ src/services/test/systemConversor.test.ts
@@ -0,0 +1,21 @@
+import { ConvertAPI } from '../systemConversor';
+
+describe('ConvertAPI', () => {
+  it('should convert Celsius to Fahrentheit correctly', async () => {
+    const converter = new ConvertAPI();
+    const amount = '20';
+    const from = 'Celsius';
+    const to = 'Fahrenheit';
+    const result = await converter.convert(amount, from, to);
+    console.log(result)
+    expect(result).toBe('20 Celsius is equal to 68 Fahrenheit')
+  });
+
+  it('should throw an error for invalid conversion', async () => {
+    const converter = new ConvertAPI();
+    const amount = '10';
+    const from = 'Celcius';
+    const to = 'INVALID';
+    await expect(converter.convert(amount, from, to)).rejects.toThrow('Unable to fetch convert');
+  });
+});
